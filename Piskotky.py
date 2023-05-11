f6d9c1a53f501439e8a339469eda0c589bea77e9
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2023-05-11 22:30:13 +0200

    Add files via upload

---
+++ Piskotky.py
@@ -0,0 +1,19 @@
+import os
+import datetime
+from time import sleep
+import random
+
+red = '\033[31m'
+green = '\033[32m'
+yellow = '\033[33m'
+blue = '\033[34m'
+#orange = '\033[033m'
+orange = '\033[38;5;208m'
+magenta = '\033[35m'
+cyan = '\033[36m'
+reset = '\033[0m'
+
+
+RC = random.choice(red, green, yellow, blue, orange, magenta, cyan)
+print("//////////// PisKoTKyY \\\\\\\\\\\\\n")
+print(f"Welcome to Piskotky{reset}")
---
+++ WiFiKicker.py
@@ -0,0 +1,32 @@
+import os
+import subprocess
+from time import sleep
+
+print(f"-------------------------------------------------")
+print(f"------(O-__-O)                     (O-__-O)------")
+print(f"         /\   Welcome to WiFiKicker   /\         ")
+print(f"        :  :                         :  :        ")
+print(f"------(O-__-O)                     (O-__-O)------")
+print(f"-------------------------------------------------")
+sleep(1)
+print(f"                  LOGO                           ")
+print(f"                  WIFIny                         ")
+print(f"                patri sem                        ")
+#print(f"                                                 ")
+#print(f"                                                 ")
+#print(f"                                                 ")
+#print(f"                                                 ")
+#print(f"                                                 ")
+#print(f"                                                 ")
+#print(f"                                                 ")
+#print(f"                                                 ")
+
+a = input("Sem nieco")
+#if a =="":
+
+#elif a =="":
+
+#elif a =="":
+
+#else:
+#    breake
