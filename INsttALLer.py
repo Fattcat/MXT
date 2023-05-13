b2ff2c37d60376ba8c28c5a35fbc24d78d9d0f12
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2023-05-13 22:58:42 +0200

    INsttALLer.py

--- INsttALLer.py
+++ INsttALLer.py
@@ -3,4 +3,11 @@ from time import sleep
 import subprocess
 
 sleep(1)
-subprosess.run(["pip "
+subprosess.run(["pip", "install", "playsound"])
+sleep(1)
+subprosess.run(["pip", "install", "pyautogui"])
+sleep(1)
+subprosess.run(["pip", "install", "os"])
+sleep(1)
+subprosess.run(["pip", "install", "pywifi"])
+sleep(1)
