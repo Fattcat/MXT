5a6635a58b36de04ae1966c349de6fae94da46f3
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2023-05-16 16:07:34 +0000

    KickerFi.py

---
+++ KickerFi.py
@@ -0,0 +1,70 @@
+import os
+import subprocess
+from time import sleep
+red = '\033[31m'
+green = '\033[32m'
+yellow = '\033[33m'
+blue = '\033[34m'
+#orange = '\033[033m'
+orange = '\033[38;5;208m'
+magenta = '\033[35m'
+cyan = '\033[36m'
+reset = '\033[0m'
+
+def Start_KickerFi():
+
+os.system("clear")
+print(f"-------------------------------------------------")
+print(f"{blue}        (--)                         (--)        {reset}")
+print(f"------({green}O{reset}-__-{green}O{reset})                     ({green}O{reset}-__-{green}O{reset})------{reset}")
+print(f"         {green}/\{reset}                           {green}/\{reset}         ")
+print(f"        :  :    Welcome to {red}KickFi{reset}    :  :        ")
+print(f"------({green}O{reset}-__-{green}O{reset})                     ({green}O{reset}-__-{green}O{reset})------{reset}")
+print(f"{blue}        (--)                         (--)        {reset}")
+print(f"-------------------------------------------------")
+sleep(2)
+os.system("clear")
+print("                                                            ")
+print("                                                            ")
+print("""                                              .!:
+                                                 .?B@&Y:     
+                                              .?#@@@@@&!    
+                                            :J#@@@@@@P~     
+                          ..:::::...      :Y&@@@@@@5^       
+                 :^!?YPGB##&&&&&&&&##P^ ^5&@@@@@&5^         
+           .^!JPB&@@@@@@@@@@@@@@@@@#?.^5@@@@@@&J:           
+       .^?P#@@@@@@@@@@@@&##BBGGGBG7.^P@@@@@@#J.:YB57:       
+    :75#@@@@@@@@@#G5J7~^:.        ~P@@@@@@#?.^P@@@@@@BJ^    
+ :7P&@@@@@@@&GJ!^.        .::   !G@@@@@@B7.  !5B&@@@@@@@BJ^ 
+:G@@@@@@&GJ~.     .^!J5PB#&P^.7B@@@@@@B!.:.     :75#@@@@@@G:
+ .?B@&P7:     ^7YG&@@@@@@P^.7B@@@@@@G! ~G@&GY!:    .!5&@G~  
+   .!:    .!YB@@@@@@@@@5^.?#@@@@@@G~ !B@@@@@@@&G?:    :^    
+        ~P&@@@@@@@&BP?:.J#@@@@@@P^   ~7JP#@@@@@@@@G!        
+        ~P@@@@@BY!:  .J&@@@@@@5^.!!~:.    ^?P&@@@&Y^        
+          ^5BJ^    ^5&@@@@@&Y:.?#@@@@&B5?^   .7PJ.          
+                 ^5@@@@@@&Y::Y&@@@@@@@@@@@#5!               
+               ~P@@@@@@#J: :??77?JYPB&@@@@@B7               
+             ~G@@@@@@#?.      .      :!YBG~                 
+          .7G@@@@@@B?.    ^5B&&#G7                          
+          ^G@@@@@B7.     ~@@@@@@@@5                         
+            ~P@G!        ?@@@@@@@@#                         
+              :           J&@@@@@G^                         
+                           .~77!^                                """)
+print("                                                            ")
+print("                                                            ")
+sleep(2)
+os.system("clear")
+
+print(f"{green}[ 1 ]{reset} {green}START KickerFi{reset}")
+print(f"{magenta}[ 2 ]{reset} {green}SETUP KickerFi{reset} {red}(NEED TO SET !){reset}")
+print(f"{orange}[ 3 ]{reset} {orange}EXIT{reset}")
+
+a = input("\nOption : ")
+#if a =="1":
+
+#elif a =="2":
+
+#elif a =="3":
+
+#else:
+#    breake
