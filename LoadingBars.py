2135c6e3e5834d442336f6ab827146707775155a
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2023-05-23 18:09:39 +0000

    Update LoadingBars.py

--- LoadingBars.py
+++ LoadingBars.py
@@ -1,2 +1,17 @@
 import os
 from time import sleep
+import time
+os.system("clear")
+sleep(0.2)
+def animate_loading():
+    bar_length = 20
+    total_iterations = 50
+    sleep_time = 0.1
+    for i in range(total_iterations + 1):
+        percent = i * 100 / total_iterations
+        filled_length = int(bar_length * i / total_iterations)
+        bar = '=' * filled_length + '>' + ' ' * (bar_length - filled_length)
+        print(f'\r[{bar}] {percent:.0f}%', end='', flush=True)
+        time.sleep(sleep_time)
+    print("\nDownload sucessfully completed ! ")
+animate_loading()
