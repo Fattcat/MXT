286d9ccf90696d4f2b40bcaa7a4844bb130fcc30
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2023-05-29 18:32:06 +0200

    Update stt-EN.py

--- stt-EN.py
+++ stt-EN.py
@@ -161,7 +161,7 @@ while True:
 
     def Animation():
         #subprocess.call(["python", "animation.py"])
-        os.system("python Piskotky.py")
+        os.system("python3 Piskotky.py")
     def REINSTALL():
         print("INSTALLING ALL PACKAGES ...\nPLEASE WAIT ...\n")
         sleep(1)
